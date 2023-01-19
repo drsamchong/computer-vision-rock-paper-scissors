e593d16fc696ecb6b84575654a627e12ff5d10d9
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-19 17:04:56 +0000

    Adds a function to check who wins baed on the computer and user choices.

--- manual.rps.py
+++ manual.rps.py
@@ -1,5 +1,6 @@
 import random
 
+
 def get_computer_choice():
     return random.choice(["Rock", "Paper", "Scissors"])
 
@@ -8,3 +9,33 @@ def get_user_choice():
     return input('Please choose "Rock", "Paper" or "Scissors"')
 
 
+def get_winner(computer_choice, user_choice):
+    if computer_choice == "Rock":
+        if user_choice == "Paper":
+            print("You won!")
+            user_wins += 1
+        elif user_choice == "Scissors":
+            print("You lost!")
+            computer_wins += 1
+        else:
+            print("It's a tie")
+
+    elif computer_choice == "Paper":
+        if user_choice == "Scissors":
+            print("You won!")
+            user_wins += 1
+        elif user_choice == "Rock":
+            print("You lost!")
+            computer_wins += 1
+        else:
+            print("It's a tie")
+            
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You won!")
+            user_wins += 1
+        elif user_choice == "Paper":
+            print("You lost!")
+            computer_wins += 1    
+        else:
+            print("It's a tie")
