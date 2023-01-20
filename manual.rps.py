fa4585fdb244690b2c68e7e3615d807839d76a85
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-20 09:54:23 +0000

    Added a function to wrap the function calls to implement a game of RPS.

--- manual.rps.py
+++ manual.rps.py
@@ -6,36 +6,46 @@ def get_computer_choice():
 
 
 def get_user_choice():
-    return input('Please choose "Rock", "Paper" or "Scissors"')
+    return input('Please choose "Rock", "Paper" or "Scissors"\n')
 
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == "Rock":
         if user_choice == "Paper":
             print("You won!")
-            user_wins += 1
+#            user_wins += 1
         elif user_choice == "Scissors":
             print("You lost!")
-            computer_wins += 1
+#            computer_wins += 1
         else:
             print("It's a tie")
 
     elif computer_choice == "Paper":
         if user_choice == "Scissors":
             print("You won!")
-            user_wins += 1
+#            user_wins += 1
         elif user_choice == "Rock":
             print("You lost!")
-            computer_wins += 1
+#            computer_wins += 1
         else:
             print("It's a tie")
             
     elif computer_choice == "Scissors":
         if user_choice == "Rock":
             print("You won!")
-            user_wins += 1
+#            user_wins += 1
         elif user_choice == "Paper":
             print("You lost!")
-            computer_wins += 1    
+#            computer_wins += 1    
         else:
             print("It's a tie")
+
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    print(f"Computer: {computer_choice} User: {user_choice}")
+    get_winner(computer_choice, user_choice)
+
+
+play()    
