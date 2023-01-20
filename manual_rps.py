e7137f81d865f01b3ed4d1018b3e11ad7b85f6ee
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-20 10:12:56 +0000

    Changed messages in get_winner function.

--- manual_rps.py
+++ manual_rps.py
@@ -17,28 +17,28 @@ def get_winner(computer_choice, user_choice):
             print("You won!")
 #            user_wins += 1
         elif user_choice == "Scissors":
-            print("You lost!")
+            print("You lost")
 #            computer_wins += 1
     elif computer_choice == "Paper":
         if user_choice == "Scissors":
             print("You won!")
 #            user_wins += 1
         elif user_choice == "Rock":
-            print("You lost!")
+            print("You lost")
 #            computer_wins += 1            
     else:
         if user_choice == "Rock":
             print("You won!")
 #            user_wins += 1
         elif user_choice == "Paper":
-            print("You lost!")
+            print("You lost")
 #            computer_wins += 1    
 
 
 def play():
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
-    print(f"Computer: {computer_choice} User: {user_choice}")
+#    print(f"Computer: {computer_choice} User: {user_choice}")
     get_winner(computer_choice, user_choice)
 
 
