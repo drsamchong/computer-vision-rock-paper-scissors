95fd94644f8fb68cff3acf73f8ad53e8ae89a441
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-20 17:50:33 +0000

    Added code to display countdown and choice on the video image.

--- camera_rps.py
+++ camera_rps.py
@@ -5,13 +5,6 @@ import numpy as np
 from keras.models import load_model
 
 
-def get_prediction(prediction):
-    predicted_choice = np.argmax(prediction[0])
-    categories = ["Rock", "Paper", "Scissors", "Nothing"]
-    print(f"You chose {categories[predicted_choice]}")
-    class_predictions = dict(zip(categories, prediction[0]))
-#    print(class_predictions)
-    return categories[predicted_choice]
 
 
 def capture_user_choice():
@@ -25,13 +18,14 @@ def capture_user_choice():
     while True:
         start_time = time.time()
         ret, frame = cap.read()
-        # cv2.imshow('frame', frame)
-        # cv2.waitKey(1)
 
         while countdown >= 0:
             ret, frame = cap.read()
-            cv2.imshow('frame', frame)
-            cv2.waitKey(1)
+            crop = frame[:, 420:1501]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(crop, str(countdown), (480, 480), font, 10, (255, 0 , 255), 4, cv2.LINE_AA)
+            cv2.imshow('crop', crop)
+            cv2.waitKey(100)
             time_now = time.time()
 
             if time_now - start_time >= 1:
@@ -41,17 +35,21 @@ def capture_user_choice():
 
         else:
             ret, frame = cap.read()
-            cv2.imshow('frame', frame)
-            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            # crop video to match Teachable Machine aspect ratio
+            crop = frame[:, 420:1501]
+            resized_frame = cv2.resize(crop, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
             normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
             data[0] = normalized_image
             prediction = model.predict(data)
-        #     # Press q to close the window
             print(prediction)
+            user_choice = get_prediction(prediction)
+            cv2.putText(crop, str(user_choice), (280, 480), font, 5, (255, 0 , 255), 4, cv2.LINE_AA)
+            cv2.imshow('crop', crop)
             cv2.waitKey(5000)
             break
 
+        # Press q to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
                 
@@ -59,43 +57,62 @@ def capture_user_choice():
     cap.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
-    user_choice = get_prediction(prediction)
+
+    if user_choice == "Nothing":
+        print("No choice detected. Try again")
+        user_choice = capture_user_choice()
     return user_choice
+
+
+def get_prediction(prediction):
+    predicted_choice = np.argmax(prediction[0])
+    categories = ["Rock", "Paper", "Scissors", "Nothing"]
+#    print(f"You chose {categories[predicted_choice]}")
+#    class_predictions = dict(zip(categories, prediction[0]))
+    return categories[predicted_choice]
+
 
 def get_computer_choice():
     return random.choice(["Rock", "Paper", "Scissors"])
 
 
 def get_user_choice():
-#    prediction = capture_user_choice()
-#    user_choice = capture_user_choice()
     return capture_user_choice()
 
+
 def get_winner(computer_choice, user_choice):
+    print(f"in get_winner: {user_choice}")
     if computer_choice == user_choice:
             print("It is a tie!")
+            winner = "neither"
     elif computer_choice == "Rock":
         if user_choice == "Paper":
             print("You won!")
+            winner = "user"
 #            user_wins += 1
         elif user_choice == "Scissors":
             print("You lost")
+            winner = "computer"
 #            computer_wins += 1
     elif computer_choice == "Paper":
         if user_choice == "Scissors":
             print("You won!")
+            winner = "user"
 #            user_wins += 1
         elif user_choice == "Rock":
             print("You lost")
+            winner = "computer"
 #            computer_wins += 1            
     else:
         if user_choice == "Rock":
             print("You won!")
+            winner = "user"
 #            user_wins += 1
         elif user_choice == "Paper":
             print("You lost")
+            winner = "computer"
 #            computer_wins += 1    
-
+    return winner
 
 def play():
     rounds_played = 0
