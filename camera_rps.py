3ab4faa3df7e4124baf66dd9d78e2e3883404e32
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-23 21:55:36 +0000

    Tidied code and tweaked timings for countdown and displaying images.

--- camera_rps.py
+++ camera_rps.py
@@ -5,15 +5,13 @@ import numpy as np
 from keras.models import load_model
 
 
-
-
 def capture_user_choice():
     model = load_model('keras_model.h5')
     # start video
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    countdown = 5
+    countdown = 3
 
     while True:
         start_time = time.time()
@@ -21,7 +19,9 @@ def capture_user_choice():
 
         while countdown >= 0:
             ret, frame = cap.read()
+            # crop to same aspect ratio as Teachable Machine training set
             crop = frame[:, 420:1501]
+            # add countdown text to the image displayed
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(crop, str(countdown), (480, 480), font, 10, (255, 0 , 255), 4, cv2.LINE_AA)
             cv2.imshow('crop', crop)
@@ -35,40 +35,38 @@ def capture_user_choice():
 
         else:
             ret, frame = cap.read()
-            # crop video to match Teachable Machine aspect ratio
             crop = frame[:, 420:1501]
             resized_frame = cv2.resize(crop, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1  # Normalize the image
             data[0] = normalized_image
             prediction = model.predict(data)
-            print(prediction)
+#            print(prediction)
             user_choice = get_prediction(prediction)
             cv2.putText(crop, str(user_choice), (280, 480), font, 5, (255, 0 , 255), 4, cv2.LINE_AA)
             cv2.imshow('crop', crop)
-            cv2.waitKey(5000)
+            cv2.waitKey(2000)
             break
 
         # Press q to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-                
+
+    if user_choice == "Nothing":
+        print("No choice detected. Try again")
+        user_choice = capture_user_choice()
+
     # After the loop release the cap object
     cap.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
 
-    if user_choice == "Nothing":
-        print("No choice detected. Try again")
-        user_choice = capture_user_choice()
     return user_choice
 
 
 def get_prediction(prediction):
     predicted_choice = np.argmax(prediction[0])
     categories = ["Rock", "Paper", "Scissors", "Nothing"]
-#    print(f"You chose {categories[predicted_choice]}")
-#    class_predictions = dict(zip(categories, prediction[0]))
     return categories[predicted_choice]
 
 
@@ -89,53 +87,50 @@ def get_winner(computer_choice, user_choice):
         if user_choice == "Paper":
             print("You won!")
             winner = "user"
-#            user_wins += 1
         elif user_choice == "Scissors":
             print("You lost")
             winner = "computer"
-#            computer_wins += 1
     elif computer_choice == "Paper":
         if user_choice == "Scissors":
             print("You won!")
             winner = "user"
-#            user_wins += 1
         elif user_choice == "Rock":
             print("You lost")
             winner = "computer"
-#            computer_wins += 1            
     else:
         if user_choice == "Rock":
             print("You won!")
             winner = "user"
-#            user_wins += 1
         elif user_choice == "Paper":
             print("You lost")
             winner = "computer"
-#            computer_wins += 1    
     return winner
 
 def play():
     rounds_played = 0
     computer_wins = 0
     user_wins = 0
-    while rounds_played != 5: # and user_wins < 3 and computer_wins < 3:
+    while rounds_played < 5: # and user_wins < 3 and computer_wins < 3:
+        computer_choice = get_computer_choice()
+        user_choice = get_user_choice()
+    #    print(f"Computer: {computer_choice} User: {user_choice}")
+        winner = get_winner(computer_choice, user_choice)
+
+        if winner =="user":
+            user_wins += 1
+        elif winner == "computer":
+            computer_wins += 1
+        rounds_played += 1
+        print(f"Computer wins: {computer_wins}. User wins: {user_wins}. Rounds played: {rounds_played}")
+
         if computer_wins == 3:
             print("Computer wins")
             break
         elif user_wins == 3:
             print("User wins")
             break
-        else:
-            computer_choice = get_computer_choice()
-            user_choice = get_user_choice()
-        #    print(f"Computer: {computer_choice} User: {user_choice}")
-            winner = get_winner(computer_choice, user_choice)
-            if winner =="user":
-                user_wins += 1
-            elif winner == "computer":
-                computer_wins += 1
-            rounds_played += 1
-            print(f"Computer wins: {computer_wins}. User wins: {user_wins}. Rounds played: {rounds_played}")
+    else:
+        print("Game over!")
 
 play()
 
